f8b0e2494db7e44f61c66984bc34035ebf4ba301
Matildevoldsen <matildeene@outlook.com>
2020-08-29 14:37:24 +0100

    Separate Label from Input component

---
+++ src/lib/Label.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
+
+class Label extends Component {
+    render() {
+        return (
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={this.props.to}>{this.props.children}</label>
+        );
+    }
+}
+
+Label.propTypes = {
+    to: PropTypes.string
+};
+
+
+export default Label;
