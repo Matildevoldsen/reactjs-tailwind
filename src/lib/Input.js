7aa85e7d56bccfba16ff2e191e549b0f5d798d27
Matildevoldsen <matildeene@outlook.com>
2020-08-29 14:37:03 +0100

    Add propTypes to Input

--- src/lib/Input.js
+++ src/lib/Input.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
 
-const Input = ({type = "text", inline = false, label, error, value, onChange}) => (
-    <div className="mb-4">
-        {label && <label className="block text-gray-700 text-sm font-bold mb-2">{label}</label>}
-        <input
-            type={type}
-            className={error ? 'react-tailwind-input--error' : 'react-tailwind-input'}
-            value={value}
-            onChange={e => onChange && onChange(e.target.value)}
-        />
-        {error && <p className="text-red-500 text-xs italic">{error}</p>}
-    </div>
-);
+class Input extends Component {
+    render() {
+        return (
+            <input
+                type={this.props.type}
+                id={this.props.id}
+                className={this.props.error ? 'react-tailwind-input--error' : 'react-tailwind-input'}
+                value={this.props.value}
+                required={!!this.props.required}
+                onChange={e => this.props.onChange && this.props.onChange(e.target.value)}
+            />
+        );
+    }
+}
+
+Input.propTypes = {
+    type: PropTypes.string,
+    id: PropTypes.string,
+    error: PropTypes.string,
+    value: PropTypes.string,
+    required: PropTypes.bool,
+};
 
 export default Input;
