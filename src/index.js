d4496ddeec401ad637780531be938e1d282194af
Matilde W. Enevoldsen <matildeene@outlook.com>
2020-10-26 16:40:16 +0000

    add export & import classes. Commented out test code on index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-import React  from 'react';
+import {render} from "react-dom";
+import './assets/index.css';
+import Input from "./lib/Forms/Input";
+import Label from "./lib/Forms/Label";
+import FormError from "./lib/Forms/FormError";
+import Button from "./lib/Button/Button";
+import Navbar from "./lib/Navbar/Navbar";
+import NavbarContainer from "./lib/Navbar/NavbarContainer";
+import Badge from "./lib/Badge/Badge";
+import NavbarLink from "./lib/Navbar/NavbarLink";
+
+export {Input};
+export {Button};
+export {Navbar};
+export {NavbarContainer};
+export {Badge};
+export {NavbarLink};
+export {FormError};
+export {Label};
+
+/*import React  from 'react';
 import {render} from "react-dom";
 import './assets/index.css';
 import Input from "./lib/Forms/Input";
@@ -30,3 +50,4 @@ function App() {
 }
 
 render(<App />, document.getElementById("root"));
+*/
